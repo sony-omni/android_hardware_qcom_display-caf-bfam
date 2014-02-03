f1474cf177c6cf9d0a89a0a650c4487a6d6a6024
Andrew Dodd <atd7@cornell.edu>
2014-02-02 20:37:24 -0500

    display-caf-bfam: Add variant filtering and update header paths
    Change-Id: I52fc91c662356702b05a57029ab5835647448e10

--- Android.mk
+++ Android.mk
@@ -4,7 +4,8 @@ else
     TARGET_USES_SDE = false
 endif
 
-display-hals := libgralloc libcopybit liblight libmemtrack libqservice libqdutils
+ifeq ($(TARGET_USES_SDE), true)
+display-hals := libgralloc libcopybit liblight libmemtrack
 
 ifeq ($(TARGET_USES_SDE), true)
     sde-libs := displayengine/libs
@@ -12,11 +13,19 @@ ifeq ($(TARGET_USES_SDE), true)
 else
     display-hals += libgenlock libhwcomposer liboverlay libhdmi
 endif
-
+else
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-bfam)
+display-hals := libgralloc libgenlock libcopybit libmemtrack
+display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
+endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
 endif
